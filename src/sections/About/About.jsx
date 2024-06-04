0598f16f8420f669e25e90a416c7e4631572044c
Rama-2020 <ramiroscerra@hotmail.com>
2024-06-04 20:36:23 +0200

    Added new icons. Finished About section

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,7 +1,7 @@
 import classes from './About.module.css'
 import aboutImg from '../../assets/images/RS.png'
-import colorModeOn from '../../assets/icons/toggle_on.svg'
-import colorModeOff from '../../assets/icons/toggle_off.svg'
+import colorModeOn from '../../assets/icons/light_mode.svg'
+import colorModeOff from '../../assets/icons/dark_mode.svg'
 import linkedinIconLight from '../../assets/icons/LinkedIn-light.svg'
 import linkedinIconDark from '../../assets/icons/LinkedIn-dark.svg'
 import githubIconLight from '../../assets/icons/github-mark-light.svg'
@@ -16,18 +16,22 @@ function About() {
     const linkedinIcon = theme === 'light' ? linkedinIconLight : linkedinIconDark;
     const githubIcon = theme === 'light' ? githubIconLight : githubIconDark;
 
-  return <section id='about' className={classes['container']}>
-    <div className={classes["colorMode"]}>
+  return (
+  <section id='about' className={classes['container']}>
+    <div className={classes["colorModeContainer"]}>
         <img 
             className={classes["aboutImage"]} 
             src={aboutImg} 
             alt="RS" 
         />
         <img 
+            className={classes["colorMode"]}
             src={themeIcon} 
             alt="Color Mode Icon"
             onClick={toggleTheme}
         />
+    </div>
+    <div className={classes["info"]}>
         <h1>
             Ramiro
             <br />
@@ -44,7 +48,7 @@ function About() {
                 <img src={githubIcon} alt="Github Icon"/>
             </a>
         </span>
-        <p>
+        <p className={classes["description"]}>
         I am passionate about combining electronics with programming to push the boundaries of what's possible. Whether it's developing embedded systems, IoT devices, or full-stack web applications, I thrive on solving complex problems with creative solutions.
         </p>
         <a href={cvFile} download='Resume'>
@@ -54,6 +58,7 @@ function About() {
         </a>
     </div>
   </section>
+  );
 }
 
 export default About
