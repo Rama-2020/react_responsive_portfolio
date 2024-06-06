85b465634dde2d67b25c148f69f51fa1740b717f
Rama-2020 <ramiroscerra@hotmail.com>
2024-06-06 22:07:15 +0200

    Added Project component

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
 import classes from './Projects.module.css'
-import {getImageURL} from '../../utils/image_util'
+import Project from '../../widgets/Project'
+
 
 function Projects({ data }) {
   return (
     <section id='projects' className={classes['container']}>
         <h1 className={classes['sectionsTitle']}>Projects</h1>
         <div className={classes['projectsContainer']}>
-        
           {data.map((project) => (
-            <div key={project.id} className={classes['project']}>
-              <a href={project.link} target='_blank'>
-              {console.log(project.imageUrl)}
-                <img className={classes['hover']} src={getImageURL(project.imageUrl)} alt={project.title} />
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
-              </a>
-            </div>
-          ),
-          )
-          }
+            <Project
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imageUrl={project.imageUrl} // Correctly pass imageUrl
+              link={project.link}    
+            />
+            ),
+            )
+            }
         </div>
 
     </section>
