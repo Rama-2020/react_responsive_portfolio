cf35d628ed18bfb86901c9241186148c967b420f
Rama-2020 <ramiroscerra@hotmail.com>
2024-06-09 17:30:57 +0200

    Added styles for skill, using the data + changing svg property

--- src/sections/Skills/Skill.jsx
+++ src/sections/Skills/Skill.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 import classes from './Skills.module.css'
 import SkillList from '../../widgets/SkillList'
 import { dummySkills } from '../../dummyData/dummyData'
+import { useTheme } from '../../common/ThemeContext'
 
 
 function Skills({ data }) {
+  const { theme, toggleTheme} = useTheme();
+
+    const colorIcon = theme === 'light' ? '#000000' : '#FFFFFF';
+
+
   return (
     <section id='skills' className={classes['container']}>
         <h1 className='sectionTitle'>Skills</h1>
         <div className={classes['skillList']}>
-          <SkillList dataSkills={dummySkills}/>
+          <SkillList dataSkills={dummySkills} color={colorIcon}/>
 
         </div>
     </section>
--- src/widgets/Project.jsx
+++ src/widgets/Project.jsx
@@ -6,7 +6,7 @@ import {getImageURL} from '../utils/image_util'
 function Project({  title, description, imageUrl, link }) {
   return (
         <a href={link} target='_blank'>
-            <img className="hover" src={getImageURL(imageUrl)} alt={title} />
+            <img className="hover" src={getImageURL(imageUrl)} alt={title}/>
             
             <h3>{title}</h3>
             <p>{description}</p>
